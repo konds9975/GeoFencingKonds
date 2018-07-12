4d0179b107e44fc4d3d949675aafe55d31cca426
Hitendra Bhoir <hitendra@hitendras-mac-mini.local>
2018-07-12 13:51:03 +0530

    Initial Commit

---
+++ GeoFencingKonds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoFencingKonds
+//
+//  Created by Hitendra Bhoir on 12/07/18.
+//  Copyright © 2018 Fortune4 Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
